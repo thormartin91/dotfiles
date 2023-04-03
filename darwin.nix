18315c6d6cbe5aa811448dc6d9c6c393f2316d97
Thor Martin Abrahamsen <--local>
2023-04-03 22:08:59 +0200

    add macOS-config and brew

--- darwin.nix
+++ darwin.nix
@@ -17,5 +17,66 @@
   };
 
   imports = [ ./home.nix ];
+ 
+  fonts = {
+    fontDir.enable = true;
+    #fonts = [ pkgs.jetbrains-mono pkgs.nerdfonts ];
+    fonts = with pkgs; [
+      (nerdfonts.override {
+        fonts = [ "FiraCode" ];
+      })
+    ];
+  };
 
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+        # NSAutomaticCapitalizationEnabled = false;
+        # NSAutomaticDashSubstitutionEnabled = false;
+        # NSAutomaticPeriodSubstitutionEnabled = false;
+        # NSAutomaticQuoteSubstitutionEnabled = false;
+        # NSAutomaticSpellingCorrectionEnabled = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+      };
+      dock = {
+        autohide = true;
+        orientation = "bottom";
+        # tilesize = 16;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        QuitMenuItem = true;
+      };
+      trackpad = {
+        TrackpadThreeFingerDrag = true;
+        Clicking = true; 
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+  };
+  #  system.defaults.NSGlobalDomain.com.apple.swipescrolldirection = false;
+
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    onActivation.cleanup = "none"; # zap;
+    onActivation.upgrade = true;
+
+    taps = [ ];
+
+    brews = [ ];
+
+    casks = [
+      "google-chrome"
+    ];
+
+    masApps = {};
+
+  };
 }
