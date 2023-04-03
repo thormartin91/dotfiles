dc1cf37dc5d8504bd21f9bf13be6660ae59f505b
Thor Martin Abrahamsen <--local>
2023-04-03 22:09:33 +0200

    add user-config

--- home.nix
+++ home.nix
@@ -20,8 +20,43 @@
         azure-cli
       ];
       programs = {
-        home-manager.enable = true;  
+        home-manager.enable = true;
+        zsh = {
+          enableCompletion = true;
+          enableAutosuggestions = true;
+          enable = true;
+          shellAliases = {
+            ls = "ls --color";
+            ll = "ls -la --color";
+          };
+        };
+        git = {
+          enable = true;
+          userName = "Thor Martin Abrahamsen";
+          userEmail = "thorma91@gmail.com";
+          aliases = {
+            prettylog = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+          };
+          extraConfig = {
+            github.user = "thormartin91";
+            color.ui = true;
+          };
+        };
+        ssh = {
+          enable = true;
+          matchBlocks = {
+            gondolin = {
+              hostname = "thormart.in";
+              user = "thorma";
+              port = 22;
+            };
+          };
+        };
+        starship.enable = true;
+        vim.enable = true;
+        vscode.enable = true;
       };
     };
+    #extraSpecialArgs = { inherit machine; };
   };
 }
